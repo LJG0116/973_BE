df27f3f7ddc1509334e550999268ccdecebc0855
윤석진 <sukjin3128@naver.com>
2022-04-13 17:31:41 +0900

    Refector "미업로드 된 파일 업로드"

---
+++ src/main/java/com/nst/fitnessu/domain/Role.java
@@ -0,0 +1,4 @@
+package com.nst.fitnessu.domain;
+
+public class Role {
+}
---
+++ src/main/java/com/nst/fitnessu/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.nst.fitnessu.repository;
+
+import com.nst.fitnessu.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
