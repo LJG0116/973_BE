554af11a7523ae220ba5df2a091f7cbaf03acb8a
윤석진 <sukjin3128@naver.com>
2022-04-13 17:10:18 +0900

    Refector "Member Repository, Member Service 수정" jpaRepository를 활용한 메서드로 변경

--- src/main/java/com/nst/fitnessu/service/MemberService.java
+++ src/main/java/com/nst/fitnessu/service/MemberService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,26 +18,26 @@ public class MemberService {
 
     //회원가입
     @Transactional
-    public Long join(Member member) {
+    public Long join(Member member){
+        //같은 이름이 있는 중복 x
         validateDuplicateMember(member);
+
         memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        //Exception
-        List<Member> findMembers = memberRepository.findByName(member.getName());
-        if (!findMembers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다");
-        }
+        memberRepository.findByEmail(member.getEmail())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
-    //회원 전체 조회
     public List<Member> findMembers(){
         return memberRepository.findAll();
     }
 
-    public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
