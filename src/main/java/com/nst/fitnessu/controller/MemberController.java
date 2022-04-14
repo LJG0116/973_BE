b89f08975613740f7dd8cdca14f635aada4a598d
이준규 <delpine07@gmail.com>
2022-04-14 15:12:29 +0900

    Feat: login구현

--- src/main/java/com/nst/fitnessu/controller/MemberController.java
+++ src/main/java/com/nst/fitnessu/controller/MemberController.java
@@ -1,13 +1,23 @@
 package com.nst.fitnessu.controller;
 
+import com.nst.fitnessu.dto.LoginRequestDto;
 import com.nst.fitnessu.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/member")
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping("/login")
+    public JSONObject login(LoginRequestDto loginRequestDto){
+        return memberService.login(loginRequestDto);
+    }
 }
--- src/main/java/com/nst/fitnessu/service/MemberService.java
+++ src/main/java/com/nst/fitnessu/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.nst.fitnessu.service;
 
+import com.nst.fitnessu.config.JwtTokenProvider;
 import com.nst.fitnessu.domain.Member;
+import com.nst.fitnessu.dto.LoginRequestDto;
 import com.nst.fitnessu.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +19,8 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;//
-
+    private PasswordEncoder passwordEncoder;
+    private JwtTokenProvider jwtTokenProvider;
     //회원가입
     @Transactional
     public Long join(Member member){
@@ -24,6 +29,33 @@ public class MemberService {
         memberRepository.save(member);
         return member.getId();
     }
+
+    public JSONObject login(LoginRequestDto loginRequestDto){
+        JSONObject jsonObject=new JSONObject();
+        try{
+            Member member = memberRepository.findByEmail(loginRequestDto.getEmail())
+                    .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+            if(!passwordEncoder.matches(loginRequestDto.getPassword(),member.getPassword())){
+                throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+            }
+            //성공 시에
+            jsonObject.put("status",200);
+            jsonObject.put("token",jwtTokenProvider.createToken(member.getEmail(),member.getRoles()));
+        }catch(IllegalArgumentException e){
+            //존재하지 않는 이메일 일때
+            if(e.getMessage().equals("가입되지 않은 E-MAIL 입니다.")){
+                jsonObject.put("status",416);
+                jsonObject.put("errorMessage","가입되지 않은 E-MAIL 입니다.");
+                //비밀번호 틀렸을 때
+            }else{
+                jsonObject.put("status",417);
+                jsonObject.put("errorMessage","잘못된 비밀번호입니다.");
+            }
+        }finally {
+            return jsonObject;
+        }
+    }
+
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByEmail(member.getEmail())
