2c8fa58336dfba9bef643db8188691dd37181b5b
윤석진 <sukjin3128@naver.com>
2022-05-14 21:50:29 +0900

    Refactor

--- src/main/java/com/nst/fitnessu/dto/post/PostListResponseDto.java
+++ src/main/java/com/nst/fitnessu/dto/post/PostListResponseDto.java
@@ -8,14 +8,14 @@ import java.time.LocalDateTime;
 @Data
 public class PostListResponseDto {
 
-    private Long id;
+    private Long postId;
     private String title;
     private String author;
     private LocalDateTime date;
     //private int viewCount;
 
     public PostListResponseDto(Post post) {
-        this.id= post.getId();
+        this.postId= post.getId();
         this.title= post.getTitle();
         this.author= post.getAuthor();
         this.date=post.getPostDate();
