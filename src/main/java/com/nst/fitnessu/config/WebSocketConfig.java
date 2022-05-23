e39eb84ce9757f9ff41c40235b0fd3371865684e
이준규 <delpine07@gmail.com>
2022-05-23 22:07:04 +0900

    ReFactor: config수정(heartbeatvalue)

--- src/main/java/com/nst/fitnessu/config/WebSocketConfig.java
+++ src/main/java/com/nst/fitnessu/config/WebSocketConfig.java
@@ -2,6 +2,8 @@ package com.nst.fitnessu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,7 +14,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");
+        config.enableSimpleBroker("/sub").
+                setTaskScheduler(taskScheduler()).
+                setHeartbeatValue(new long[] {3000L, 3000L});
         config.setApplicationDestinationPrefixes("/pub");
     }
     //.addInterceptors(new HttpHandshakeInterceptor())
@@ -20,4 +24,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
+
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.initialize();
+        return taskScheduler;
+    }
+
 }
